a47a53c9d45e8aadc526d6f8e3ff2f8e9e8f5d72
Nancy-1311 <nancykhundia44@gmail.com>
2026-03-18 23:23:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
+const recipeRoutes = require("./routes/recipeRoutes");
+
+require("dotenv").config();
 
 const app = express();
 
-// CORS CONFIG (IMPORTANT FOR DEPLOYMENT)
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-  })
-);
-
-// Middleware
 app.use(express.json());
 
-// DB Connection
-connectDB();
 
-// Routes
-app.use("/api/auth", authRoutes);
-
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
+app.use("/recipes", recipeRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
